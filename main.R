896c892a8a2f92d74c7c3e2c47e4241a2ee23bbc
Murray Lyne <murraylyne2@gmail.com>
2019-06-04 19:37:47 +0100

    managed to create a model and predict it on itself. trying to follow harsha's code examples causes errors. Need to discuss this on thursday.

--- main.R
+++ main.R
@@ -1,1 +1,32 @@
 library(jsonlite)
+library(kernlab)
+library(caret)
+library(earth)
+
+
+cardata <- read.csv("data/car_data.csv")
+
+carFrame <- data.frame(data)
+
+sample = floor(0.70 * nrow(carFrame))
+
+set.seed(123)
+
+trainindex <- sample(seq_len(nrow(carFrame)), size = sample)
+
+train <- cardata[trainindex, ]
+
+test <- cardata[-trainindex, ]
+
+
+#TRAIN DATASET
+
+fit <- ksvm(train)
+
+
+print(fit)
+
+predictions <- predict(fit)
+
+print(predictions)
+
