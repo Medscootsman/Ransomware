3de515bf6191aa4d205bdba382fa48570070e971
Murray Lyne <murraylyne2@gmail.com>
2019-07-05 11:48:39 +0100

    http feature extractor + early pcaps spider

--- data/parser.py
+++ data/parser.py
@@ -1,19 +1,131 @@
 import dpkt
+import csv
+import time
+import socket
+import os
+import shutil
+import fileinput
+import codecs
 
-f = open("loocipher.pcapng", 'rb')
-http_data = ["method", "uri", "code", "domain"]
-pcap = dpkt.pcapng.Reader(f)
+#get all files in directory
+fileList = []
+pcaps = []
 
-for ts, buf in pcap:
-    eth = dpkt.ethernet.Ethernet(buf)
-    ip = eth.data
-    tcp = ip.data
-    try:        
-        if(tcp.dport == 80):
-            http = dpkt.http.Request(tcp.data)
-            print(http.method)
-    except Exception as e:
-        print("ERROR: " + str(e))
+def flagToFactor(flag):
+    switcher = {
+        2: "SYN",
+        16: "ACK",
+        24: "ACKPSH",
+    }
+    
+    return switcher.get(flag, "INVALIDFLAG!")
+
+fileList = [f for f in os.listdir(".") if (os.path.isfile(f))]
+print(fileList)
+
+for file in fileList:
+    if(file.endswith('.pcap') or file.endswith('.pcapng')):
+        pcaps.append(file)
+        print("pcap added")
         
+print(pcaps)
 
-f.close()
+totalFiles = 1
+
+for pcap in pcaps:
+    totalFiles = totalFiles + 1
+    f = open(pcap, 'rb')
+    
+    #check file is actually a pcap file
+    if(f.name.endswith("pcapng")):
+        pcap = dpkt.pcapng.Reader(f)
+        isFile = True
+    elif(f.name.endswith("pcap")):
+        pcap = dpkt.pcap.Reader(f)
+        isFile = True
+    else:
+        print("Not a pcap file")
+        isFile = False
+        
+    
+    if(isFile):    
+        http_data = [["type", "sourceIP", "destIP", "bytes", "sourceport", "method", "body", "bodySize", "version", "uri", "uri_size", "agent", "version", "host", "sum", "flags"]]
+        
+        
+        for ts, buf in pcap:
+            eth = dpkt.ethernet.Ethernet(buf)
+            ip = eth.data
+            tcp = ip.data
+            
+            #print(dir(tcp))
+            #print(dir(ip))
+            #print(len(buf))
+            
+            try:
+                if(tcp.dport == 80):
+                    tsum = tcp.sum
+                    
+                    flag = tcp.flags
+                    
+                    flagFactor = flagToFactor(flag)
+                    print(flagFactor)
+                    
+                    #get http object
+                    http = dpkt.http.Request(tcp.data)
+                    
+                    #get parameters
+                    
+                    method = http.method           
+                    body = http.body
+                    bodySize = len(body)
+                    version = http.version
+                    url = http.uri
+                    url_size = len(http.uri)
+                    headers = http.headers
+                    host = headers.get("host", "")
+                    #print(url)
+                    agent = headers.get("user-agent", "")
+                    if(agent == ""):
+                        agent = "TOR"
+                    
+                    #Get ip related information
+                    
+                    sourceIP = socket.inet_ntoa(ip.src)
+                    destinationIP = socket.inet_ntoa(ip.dst)
+                    byte = len(buf)
+                    sourcePort = tcp.sport
+                    
+                    #store parameters
+                    data = ["malicious", sourceIP, destinationIP, byte, sourcePort, method, body, bodySize, version, url, url_size, agent, version, host, tsum]
+                    
+                    http_data.append(data)
+            except Exception as e:
+                print(str(e))
+                
+                
+        f.close()
+        
+        
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        if(http_data is not None):
+            with open("http_traffic " + str(totalFiles) + timestr + ".csv", "w", newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                csvname = csvfile.name
+                for row in http_data:
+                    writer.writerow(row)
+                    
+                csvfile.close()
+                
+            if(os.path.isdir("httpdata") == False):
+                os.mkdir("httpdata")
+        
+            shutil.move(csvname, "httpdata/" + csvname)
+        
+            print("parsed " + csvname + " successfully")
+        else:
+            print("No HTTP data found")
+        
+    http_data = []
+    
+print(totalFiles)
+
--- main.R
+++ main.R
@@ -13,17 +13,36 @@ sample = floor(0.70 * nrow(carFrame))
 
 set.seed(123)
 
-trainindex <- sample(seq_len(nrow(carFrame)), size = sample)
+ind <- sample(2, nrow(cardata), replace = TRUE, prob = c(0.7, 0.3))
+train <- cardata[ind==1,]
+test <- cardata[ind==2,]
 
-train <- cardata[trainindex, ]
+train$price = as.numeric(train$price)
 
-test <- cardata[-trainindex, ]
+train$horsepower = as.numeric(train$horsepower)
 
+str(train)
+
+library(randomForest)
+
+set.seed(222)
+
+rf <- randomForest(make~., data=train,
+                  ntree=1000,
+                  mtry=8,
+                  important=TRUE,
+                  proximity=TRUE,
+                  na.action=na.exclude)
+
+print(rf)
+
+p1 <- predict(rf, train)
+
+plot(rf)
 
 #TRAIN DATASET
 
 fit <- ksvm(aspiration~., train)
-
 
 print(fit)
 
