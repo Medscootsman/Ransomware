21cc701c3de7b651e091e61ea7dc07627b496e45
Murray Lyne <murraylyne2@gmail.com>
2019-07-12 20:15:18 +0100

    http machine learning

--- httpdatset.R
+++ httpdatset.R
@@ -1,9 +1,20 @@
 #install.packages("plyr")
 #install.packages("readr")
+#install.packages("dplyr")
+#install.packages("iptools")
 library(plyr)
 library(readr)
 library(randomForest)
 library(dplyr)
+library(iptools)
+library(jsonlite)
+library(caret)
+library(earth)
+library(keras)
+library(caTools)
+library(RWeka)
+
+#PREPARATION
 set.seed(123)
 files <- list.files(path = "data/httpdata", pattern = "*.csv", full.names = TRUE)
 
@@ -13,30 +24,57 @@ data=data %>% mutate_if(is.character, as.factor)
 
 data$uri_size <- as.integer(as.character(data$uri_size))
 
-data$bytes <- as.double(as.character(data$bytes))
+data$bytes <- as.integer(as.character(data$bytes))
 
-data$sourceport <- as.double(as.character(data$sourceport))
+data$sum <- as.integer(as.character(data$sum))
 
-data$sourceIP <- as.double(as.character(data$sourceIP))
+data$tcpwin <- as.integer(as.character(data$tcpwin))
 
-data$destIP <- as.double(as.character(data$destIP))
+data$bodySize <- as.integer(as.character(data$bodySize))
 
-data$host <- as.character(data$host)
+data$hostLength <- as.integer(as.character(data$hostLength))
 
-data$uri <- as.character(data$uri)
+data$sourceport <- as.integer(as.character(data$sourceport))
 
-data$body <- as.character(data$body)
+data$sourceIP <- iptools::ip_to_numeric(as.character(data$sourceIP))
 
-train <- data[1:1800,]
-test <- data[1801:2911,]
+data$destIP <- iptools::ip_to_numeric(as.character(data$destIP))
+
+data$host <- as.numeric(data$host)
+
+dataV2 <- data.frame(data$type, data$sourceIP, data$destIP, data$bytes, data$sourceport, data$method, data$bodySize, 
+                     data$version, data$fileType, data$uri_size, data$agent, data$hostLength, data$onion, data$comDomain, 
+                     data$sum, data$flags, data$tcpwin)
 
 
-data <- na.exclude(train)
-train <- select(train, -c(14))
+ind <- sample(2, nrow(dataV2), replace = TRUE, prob = c(0.7, 0.3))
+train <- dataV2[ind==1,]
+test <- dataV2[ind==2,]
 
 summary(train)
 
 levels(train$agent)
 
-rf <- randomForest(type ~., data = train, na.action = na.exclude)
+#randomforest
+rf <- randomForest(data.type ~., data = train, na.action = na.exclude, ntree = 800, mtry = 8)
+
+print(rf)
+
+p1 <- predict(rf, newdata = test)
+
+rftable <- table(test$data.type, p1)
+
+print(rftable)
+
+#J48
+j48res <- J48(data.type~., train)
+
+test.pred <- predict(j48res, newdata = test)
+
+print(j48res)
+
+j48table <- table(test$data.type, test.pred)
+
+print(j48table)
+#naive bayes
 
